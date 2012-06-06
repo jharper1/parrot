92cd9ccb512401c1fa866426da4402d534383c5c
Justin Harper <justinlharper@hotmail.com>
2012-06-06 14:46:27 -0400

    update to include

---
+++ include/parrot/security.h
@@ -0,0 +1,14 @@
+/* security.h
+ *  Copyright (C) 2001-2012, Parrot Foundation.
+ *  Overview:
+ *     The security handles limiters, permissions and restrictions of the parrot system.
+ */
+
+
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4 cinoptions='\:2=2' :
+ */
